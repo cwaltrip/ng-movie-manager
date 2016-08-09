7646683eb9b10c78183c87db881865accc9578ef
cwaltrip <cwaltrip@gmail.com>
2016-08-09 11:10:32 -0500

    Commit before some Bootstrap style experiments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,18 +21,22 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/main', {
+        title: 'My Movie Collection',
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
       .when('/about', {
+        title: 'About | My Movie Collection',
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
       .when('/search', {
+        title: 'Search | My Movie Collection',
         templateUrl: 'views/search.html',
         controller: 'SearchCtrl'
       })
       .when('/movie/:movieId', {
+        title: 'Movie | My Movie Collection',
         templateUrl: 'views/movie.html',
         controller: 'MovieCtrl'
       })
